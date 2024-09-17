6dc1ce111bad67bcc54f859ea943083e97da8685
Gustavo Martins <gustavo_mf1@hotmail.com>
2024-09-17 14:38:50 -0300

    Padrao dto para tecnico

--- src/main/java/com/gustavo/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/gustavo/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.gustavo.helpdesk.resources;
 
 import com.gustavo.helpdesk.domain.Tecnico;
+import com.gustavo.helpdesk.domain.dtos.TecnicoDTO;
 import com.gustavo.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,9 @@ public class TecnicoResource {
     @Autowired
     private TecnicoService service;
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
         Tecnico obj = service.findById(id);
 
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 }
