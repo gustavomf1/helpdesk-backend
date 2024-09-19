df72ce5e45690e81026210e51e64071d82197573
Gustavo Martins <gustavo_mf1@hotmail.com>
2024-09-19 11:15:04 -0300

    metodo put para tecnico

--- src/main/java/com/gustavo/helpdesk/domain/Pessoa.java
+++ src/main/java/com/gustavo/helpdesk/domain/Pessoa.java
@@ -3,6 +3,7 @@ package com.gustavo.helpdesk.domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gustavo.helpdesk.domain.enums.Perfil;
 import jakarta.persistence.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -19,6 +20,7 @@ public abstract class Pessoa implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
     protected String nome;
+    @CPF
     @Column(unique = true)
     protected String cpf;
     @Column(unique = true)
